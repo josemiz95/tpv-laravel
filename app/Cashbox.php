bb9d0ff78ab5210c16393c11cba406de19544983
Josemiz95 <josemiz95@gmail.com>
2020-05-01 12:17:09 +0200

    Modificacion modelo cashbox

--- app/Cashbox.php
+++ app/Cashbox.php
@@ -6,14 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Cashbox extends Model
 {
-    //
+    // Modelo de Cashbox en TPV
     protected $table = 'cashboxes';
 
     public function user(){
+        // Relacion con Usuarios (N-1)
         return $this->belongsTo('App\User');
     }
 
     public function tickets(){
+        // Relacion con tickets (1-N)
         return $this->hasMany('App\Titcket', 'id', 'cashbox_id');
     }
 }
