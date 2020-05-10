4ae5bfc9af4ab2cfdaf1af4c912b457fba315139
Josemiz95 <josemiz95@gmail.com>
2020-05-10 15:32:34 +0200

    Guardar estado del menu

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,22 @@
+// Variables globales
+let menu = document.querySelector('.appMenu');
+
 document.addEventListener("DOMContentLoaded",function(){
+
+    // INIT
+    if(window.localStorage.getItem('smallMenu')=="false"){
+        menu.classList.toggle('smallMenu');
+    };
+
+    // EVENT LISTENERS
 
     document.getElementById('userMenu').addEventListener("click", function(){
         document.querySelector('.userMenu .menu').classList.toggle('hide');
     });
 
     document.getElementById('toggleMenu').addEventListener("click", function(){
-        // document.querySelector('.appMenu').classList.toggle('hide');
-        document.querySelector('.appMenu').classList.toggle('smallMenu');
+        menu.classList.toggle('smallMenu');
+        window.localStorage.setItem('smallMenu', menu.classList.contains('smallMenu'));
     });
     
 })
